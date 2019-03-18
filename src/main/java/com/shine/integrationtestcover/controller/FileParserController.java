be95b7869dd4876aa22403a9b00a2a87c5f7cb24
WHQ <2283149823@qq.com>
2019-03-18 22:25:30 +0800

    ImproveGraph

--- src/main/java/com/shine/integrationtestcover/controller/FileParserController.java
+++ src/main/java/com/shine/integrationtestcover/controller/FileParserController.java
@@ -27,6 +27,7 @@ public class FileParserController {
     public HashMap<String, Object> getInvokeRelationship(@RequestParam String name){
         graphService.setFilename(name);
         graphService.setPath(baseConfig.getUploadedFilePath());
+       // graphService.setPath("C://Users//22831//Desktop");
         graphService.initiate();
         ArrayList edges=graphService.getEdges();
         ArrayList<HashMap<String, Object>> vertex=graphService.getVertex();
