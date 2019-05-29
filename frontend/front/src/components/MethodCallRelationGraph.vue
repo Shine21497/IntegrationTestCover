bdc3313212c4a768104d6aafb71a1eb8cc93b338
YYYang <1225248841@qq.com>
2019-05-29 14:02:22 +0800

    Update MethodCallRelationGraph.vue

--- frontend/front/src/components/MethodCallRelationGraph.vue
+++ frontend/front/src/components/MethodCallRelationGraph.vue
@@ -159,7 +159,7 @@
                                         </el-select>
                                     </el-form-item>
                                     <el-form-item label="方法选择">
-                                        <el-select ref="selectTestMethod" filterable  :disabled="Object.entries(testCaseMap).length == 0 || selectTestForm.allTestCases.length == 0" v-model="selectTestForm.selectedTestCase" placeholder="请选择测试方法">
+                                        <el-select ref="selectTestMethod" filterable  :disabled="Object.entries(testCaseMap).length == 0 || selectTestForm.allTestCases.length == 0" v-model="selectTestForm.selectedTestCase" placeholder="请选择测试方法" @change="getTestMethod">
                                             <el-option
                                                     v-for="item in selectTestForm.allTestCases"
                                                     :key="item"
@@ -193,7 +193,7 @@
                                         </el-select>
                                     </el-form-item>
                                     <el-form-item>
-                                        <div id="branches">分支总数：0；</div>
+                                        <div id="branches">分支总数：{{branchnum}}；</div>
                                         <div id="usecase">执行用例数：{{usecasenum}}；</div>
                                         <div id="uncoverbranch">未覆盖分支数：{{uncoverlength}}；</div>
                                         <div id="coverrate">覆盖率：{{coverrate}}%：</div>
@@ -215,6 +215,16 @@
                                   <el-form-item label="新节点">
                                     <el-input type="textarea" v-model="newnode" placeholder="请输入新节点名称"></el-input>
                                   </el-form-item>
+                                  <el-form-item label="类型选择">
+                                                                          <el-select v-model="selectednodetype" filterable placeholder="请选择节点类型" @change="getnodetype()">
+
+                                                                                      <el-option label="数据库" value="1"> </el-option>
+                                                                                      <el-option label="外设" value="2"> </el-option>
+                                                                                      <el-option label="前端" value="3"> </el-option>
+                                                                                      <el-option label="其他系统" value="4"> </el-option>
+
+                                                                          </el-select>
+                                                                      </el-form-item>
                                   <el-form-item>
                                    <el-button type="primary" @click="createNewNode()">立即创建</el-button>
                                   </el-form-item>
@@ -314,7 +324,7 @@ import { Promise } from 'q';
                 taskId:'',
                 taskType:'',  // "many" 和 "one" 
                 toggle:true,
-                Isfirstnode:true,
+                selectednodetype:'',
                 history:{
                     packages:[],
                     packagesCall:[]
@@ -374,7 +384,7 @@ import { Promise } from 'q';
                 
                 trans.x = (Math.round(width/2) - node.x) * trans.k
                 trans.y = (Math.round(height/2) - node.y) * trans.k
-                
+
                 this.g.attr('transform', trans)
             },
             //显示用例测试结果
@@ -382,20 +392,25 @@ import { Promise } from 'q';
                 // 记录正在显示的测试结果，cancelshow 的时候根据这个来
                 //var type="one";
                 //var TestResult=["com.example.demo.controller.Test1:calculate call com.example.demo.controller.Test1:doublevalue"];
-                this.$nextTick(() => {
+                /*this.$nextTick(() => {
                     this.usecasenum = TestResult.length;
                     console.log(this.usecasenum);
-                })
+                })*/
                 this.TestResult = TestResult;
-                for(let index in TestResult){
-                    var result=TestResult[index].split(" ");
-                    //如果是单个结果
-                    if(type === 'one'){
-                        //线的流动效果和节点效果
+                if(type==='one'){
+                    for (let index in TestResult)
+                    {
+                        var result=TestResult[index].split(" ");
+                        if(index===0){
+                            moveFirstnode(result[0]);
+                        }
                         this.changeSingleLine(result[0],result[2]);
                     }
-                    //如果多个结果
-                    else{
+                }
+                if(type==='many')
+                {
+                for(let index in TestResult){
+                        var result=TestResult[index].split(" ");
                         this.changeMultipleLine(result[0],result[2]);
                     }
                 }
@@ -410,25 +425,22 @@ import { Promise } from 'q';
                         d3.select('#eachline' + line_id).classed('showsinglepath',true)
                         this.changeNode(SourceName);
                         this.changeNode(TargetName);
-                        if(Isfirstnode)
-                        {
-                            var node = this.findNodeByName(SourceName)
+                    }
+                    var callrelation=this.relation.links[index].source.name+" CALL "+this.relation.links[index].target.name;
+                    if(this.testCaseMap.indexOf(callrelation)<0)
+                        this.uncoverfullname.push(callrelation);
+                };
+                this.setUncover();
+            },
+
+            moveFirstnode(name){
+                    var node = this.findNodeByName(name)
                             var trans = this.tempTrans
                             trans.k = 1;
                             this.g.attr('transform',trans);
                             trans.x = (510 - node.x) * trans.k
                             trans.y = (300 - node.y) * trans.k
                             this.g.attr('transform', trans)
-                            Isfirstnode=false
-                        }
-                       
-                    }
-                    else
-                    {
-                    this.uncoverfullname.push(this.relation.links[index].source.name+" call "+this.relation.links[index].target.name);
-                    }
-                };
-                //this.setUncover();
             },
 
             //多个用例测试结果
@@ -441,12 +453,11 @@ import { Promise } from 'q';
                         this.changeNode(SourceName);
                         this.changeNode(TargetName);
                     }
-                     else
-                      {
-                      this.uncoverfullname.push(this.relation.links[index].source.name+" call "+this.relation.links[index].target.name);
-                      }
-               };
-               //this.setUncover();
+                    var callrelation=this.relation.links[index].source.name+" CALL "+this.relation.links[index].target.name;
+                    if(this.testCaseMap.indexOf(callrelation)<0)
+                        this.uncoverfullname.push(callrelation);
+                };
+               this.setUncover();
             },
             //给节点加上边界效果
             changeNode(Name) {
@@ -465,7 +476,6 @@ import { Promise } from 'q';
                     this.cancelLine(result[0],result[2]);
                     this.cancelNode(result[0]);
                     this.cancelNode(result[2]);
-
                 }
             },
             //取消连线效果
@@ -497,18 +507,25 @@ import { Promise } from 'q';
                     }
                 }
             },
-
+            getnodetype(){
+            console.log(this.selectednodetype);
+            },
             getTestClass(prov) {
                 this.selectTestForm.selectedTestCase = '';
                 this.selectTestForm.allTestCases =  this.testCaseMap[this.selectTestForm.selectedTestProject.split('.')[0]][prov]
+                this.uncoverfullname=[];
             },
             getTestProject(prov) {
                 this.selectTestForm.selectedTestCase = '';
                 this.selectTestForm.selectedTestClass = '';
+                this.uncoverfullname=[];
 
                 var prjName = prov.split('.')[0];
                 // prov is "demo.jar" but testCaseMap is {"demo":{...}}
                 this.showTestClass(prjName,1)
+            },
+            getTestProject() {
+                this.uncoverfullname=[];
             },
             showTestClass(prjName,time){
                 if(this.testCaseMap[prjName])
@@ -578,6 +595,7 @@ import { Promise } from 'q';
             //获取未覆盖信息
             setUncover(){
                 console.log(this.uncoverfullname)
+                this.uncover=[];
                 for(let index in this.uncoverfullname)
                 {
                 var temp=this.uncoverfullname[index].split(" ");
@@ -586,6 +604,7 @@ import { Promise } from 'q';
                 this.uncover.push(A+" call "+B);
                                 //console.log(A+" call "+B);
                 }
+                // this.uncoverfullname=[];
             },
             //定位到未覆盖边
             gotoUncover(){
@@ -620,6 +639,7 @@ import { Promise } from 'q';
                                 }
                             };
             //var addnode={name:"",type:0,index:7,vx:-0.0034354444444444444,vy:-0.0003758444444444444,x:171.4738754444444,y:45.5769444444444};
+
             var addnode={name:"",type:1,index:'',vx:'',vy:'',x:'',y:''};
             console.log(node.vx);
             console.log(node.vy);
@@ -629,6 +649,8 @@ import { Promise } from 'q';
             addnode.x=node.x-1.0000000000000000000;
             addnode.y=node.y-1.0000000000000000000;
             addnode.index=this.relation.nodes.length;
+            addnode.type=this.selectednodetype;
+            console.log(addnode.type)
             var addline={index:7,source:[],target:[]}
             addline.source=node;
             addline.target=addnode;
@@ -709,9 +731,22 @@ import { Promise } from 'q';
                 let _this = this;
                 getInvokingResults(_this.taskId).then(response => {  // 这里的 response 为测试用例的结果，一个 list
                     // 展示测试用例的结果
-                    _this.usecasenum = response.length;
-                    this.uncoverlength = this.relation.links.length-this.usecasenum;
-                    this.coverrate=(this.usecasenum/this.relation.links.length)*100;
+                    //_this.usecasenum = response.length;
+                    this.branchnum=this.relation.links.length;
+                    if(this.selectTestForm.selectedTestClass=="allClass")
+                    {
+                        this.usecasenum=this.testCaseMap[this.selectedTestProject].length;
+                    }
+                    else
+                    {
+                    if(this.selectTestForm.selectedTestCase=="allMethods")
+                    {
+                         this.usecasenum=this.testCaseMap[this.selectedTestProject][this.selectedTestClass].length;
+                    }
+                    else this.usecasenum=1;
+                    }
+                    this.uncoverlength = this.uncover.length;
+                    this.coverrate=((this.branchnum-this.uncoverlength)/this.branchnum)*100;
                     console.log(response)
                     _this.showTestResult(response,_this.taskType)
                 });
