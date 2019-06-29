9a4892fe0ea74550f418468e8cc6581772e9f697
Golden Dragon <zou_406143883@qq.com>
2019-06-29 22:45:57 +0800

    回归测试上传文件带参数

--- frontend/front/src/components/MethodCallRelationGraph.vue
+++ frontend/front/src/components/MethodCallRelationGraph.vue
@@ -326,7 +326,7 @@
                             :on-success="uploadSucc"
                             :limit="3"
                             :auto-upload="false"
-                            :data="regression.info.oldVersion">
+                            :data="{ projectName: regression.info.oldVersion }">
                             <el-button slot="trigger" size="small" type="primary">选择要上传的版本Jar包</el-button>
                             <el-button style="margin-left: 10px;" size="small" type="success" @click="UploadJars">上传</el-button>
                         </el-upload>
